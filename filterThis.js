9293219fa74e29dcb45319ab718193cdaef83403
Derek Ortiz <ortizder@gmail.com>
2015-04-19 01:12:12 -0400

    Implement shouldHide option and keep track of hide target parents of selected filter targets to allow for multiple tags in one container to be filtered and considered by the hide function.

--- filterThis.js
+++ filterThis.js
@@ -33,19 +33,21 @@
         inputText = event.currentTarget.value;  
         // if there is input search for and highlight string
         regEx = new RegExp(escapeRegExp(inputText), 'i');
+        $lastParentToHide = '';
         $target.each(function(iteration) {
           $element = $(this);
+          $parentToHide = $element.closest(instance.config.filterHide);
+          if (!($parentToHide[0] === $lastParentToHide[0])) {
+            $parentToHide.data('filter-matches', 0);
+            $lastParentToHide = $parentToHide;
+          }
           oldString = $.trim($element.text());
           if (inputText != '') {
             newString = oldString.replace(regEx, "<span class='filter-this-highlighter'>"+'$&'+"</span>");
             $element.html(newString);
-            if (oldString === newString) {
-              // case when no match is found on target
-              $element.closest(instance.config.filterHide).hide();
-            }
-            else {
+            if (!(oldString === newString)) {
               // when match if found make sure it is visible
-              $element.closest(instance.config.filterHide).show();
+              $parentToHide.data('filter-matches', $parentToHide.data('filter-matches')+1);
               if (instance.config.shouldHighlight === true) {
                 $element.children(".filter-this-highlighter").css("background", instance.config.highlightColor);
               }
@@ -54,11 +56,22 @@
           else {
             // nothing searched: replace to clear highlight and show element
             $element.html(oldString);
-            $element.closest(instance.config.filterHide).show();
+          }
+          if(instance.config.shouldHide) {
+            instance.hide($parentToHide, inputText);
           }
         });
       });
       return instance;
+    },
+
+    hide: function(parentToHide, inputText) {
+      if((parentToHide.data('filter-matches') > 0) || (inputText === '')) {
+        parentToHide.show();
+      }
+      else{
+        parentToHide.hide();
+      }
     }
   }
 
